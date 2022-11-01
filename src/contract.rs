bdf7ee7897f9a7e433953da53f567ec66b0f6945
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-11-01 11:59:51 -0400

    personalize

--- src/contract.rs
+++ src/contract.rs
@@ -1,41 +1,80 @@
 #[cfg(not(feature = "library"))]
-use cosmwasm_std::entry_point;
+use cosmwasm_std::{StdError, entry_point};
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
+use cw2::{get_contract_version, set_contract_version};
 
+use crate::coin_helpers::assert_sent_exact_coin;
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
-/*
-// version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cw-minimal";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
+const ADMIN: &str = "crypto_address";
+const CRYPTO: &str = "ucrypto";
 
-#[cfg_attr(not(feature = "library"), entry_point)]
+#[entry_point]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info: MessageInfo,
+    info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
-}
+    if info.sender != ADMIN {
+        return Err(ContractError::Unauthorized {  });
+    }
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    let validated_admin = deps.api.addr_validate(ADMIN)?;
+    let config = Config {
+        admin: validated_admin.clone(),
+    };
+    CONFIG.save(deps.storage, &config)?;
+    Ok(Response::new()
+        .add_attribute("action", "instantiate")
+        .add_attribute("admin", validated_admin.to_string()))
+}   
 
-#[cfg_attr(not(feature = "library"), entry_point)]
+#[entry_point]
 pub fn execute(
     _deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
     _msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
+    match msg {
+        ExecuteMsg::Example {
+            data,
+            data1,
+            data2,
+        } => execute_example(deps, env, info, data, data1, data2),
+    }
     unimplemented!()
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
+#[entry_point]
 pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
     unimplemented!()
 }
 
-#[cfg(test)]
-mod tests {}
+//pagination
+const MAX_LIMIT: u32 = 30;
+const DEFAULT_LIMIT: u32 = 10;
+
+
+#[entry_point]
+pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+    let ver = get_contract_version(deps.storage)?;
+    if ver.contract != CONTRACT_NAME {
+        return Err(StdError::generic_err("Can only upgrade from same type").into());
+    }
+
+    #[allow(clippy::cmp_owned)]
+    if ver.version > (*CONTRACT_VERSION).to_string() {
+        return Err(StdError::generic_err("Must upgrade from a lower version").into());
+    }
+
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    Ok(Response::default()
+        .add_attribute("action", "migration")
+        .add_attribute("version", CONTRACT_VERSION)
+        .add_attribute("contract", CONTRACT_NAME))
+}
