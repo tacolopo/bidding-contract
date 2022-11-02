f7619202544545617737863f3adee7cf879a9052
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-11-02 11:24:38 -0400

    finally

---
+++ wtf/src/contract.rs
@@ -0,0 +1,74 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::{entry_point, StdError};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::{get_contract_version, set_contract_version};
+
+use crate::coin_helpers::assert_sent_exact_coin;
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use crate::state::{Config, CONFIG};
+
+const CONTRACT_NAME: &str = "crates.io:cw-minimal";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+const ADMIN: &str = "crypto_address";
+const CRYPTO: &str = "ucrypto";
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    if info.sender != ADMIN {
+        return Err(ContractError::Unauthorized {});
+    }
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    let validated_admin = deps.api.addr_validate(ADMIN)?;
+    let config = Config {
+        admin: validated_admin.clone(),
+    };
+    CONFIG.save(deps.storage, &config)?;
+    Ok(Response::new()
+        .add_attribute("action", "instantiate")
+        .add_attribute("admin", validated_admin.to_string()))
+}
+
+#[entry_point]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    match msg {
+        // ExecuteMsg::Example { data, data1, data2 } => {
+        //     execute_example(deps, env, info, data, data1, data2)
+        }
+}
+
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        // QueryMsg::Example {} => query_msg_example(deps, env),
+    }
+}
+
+#[entry_point]
+pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+    let ver = get_contract_version(deps.storage)?;
+    if ver.contract != CONTRACT_NAME {
+        return Err(StdError::generic_err("Can only upgrade from same type").into());
+    }
+
+    #[allow(clippy::cmp_owned)]
+    if ver.version > (*CONTRACT_VERSION).to_string() {
+        return Err(StdError::generic_err("Must upgrade from a lower version").into());
+    }
+
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    Ok(Response::default()
+        .add_attribute("action", "migration")
+        .add_attribute("version", CONTRACT_VERSION)
+        .add_attribute("contract", CONTRACT_NAME))
+}
